3f71586368dbd3883e41fbfb3753b39c792665cd
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-15 14:17:07 +0200

    add api to tours

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const express = require("express")
+const fs = require("fs");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>{
-  res.status(200).send("Hello from the server side")
-})
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side", app: "Natrous" });
+// });
 
-const port = 3000
-app.listen(port,()=>{
+// app.post("/",(req, res)=>{
+//   res.send("you can send to this endpoint.")
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
   console.log(`App running on port: ${port}...`);
-})
+});
