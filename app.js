27591ec4f3ec768a07c2e82f48bb008081530767
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-16 01:54:18 +0200

    add delete method

--- app.js
+++ app.js
@@ -68,6 +68,18 @@ app.patch("/api/v1/tours/:id", (req, res) => {
     },
   });
 });
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "failed",
+      message: "invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
