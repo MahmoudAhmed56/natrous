2f1e26c68ae6c4811ac88306095d8b450e538fe6
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-16 23:09:41 +0200

    add Environment Variables

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const app = express();
 
-const tourRouter = require("./routes/tourRoutes")
-const userRouter = require("./routes/userRoutes")
 
-app.use(morgan("dev"));
+console.log(process.env.NODE_ENV);
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan("dev"));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log("hello from middleware");
   next();
@@ -17,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/v1/tours",tourRouter)
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express")
 
-const {getAllTours,createTour,getTour,updateTour,deleteTour,checkID} = require("./../controllers/tourController")
+const {getAllTours,createTour,getTour,updateTour,deleteTour,checkID,checkBody} = require("./../controllers/tourController")
 const router = express.Router()
 router.param("id",checkID)
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(getAllTours).post(checkBody,createTour);
 router
   .route("/:id")
   .get(getTour)
